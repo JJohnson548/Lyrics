204b88634e3a1603e4cb4d406e931f21718c7063
JJohnson548 <jjohnson548@hawkmail.hccfl.edu>
2024-08-29 00:37:25 +0000

    Unit test 3

--- main.cs
+++ main.cs
@@ -1,10 +1,13 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Oh, well imagine /");
-    Console.WriteLine ("As I'm pacing the pews in a church corridor /");
-    Console.WriteLine ("And I can't help but to hear /");
-    Console.WriteLine ("No, I can't help but to hear an exchanging of words...");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Coming out of my cage");
+
+        Console.WriteLine("And I've been doing just fine");
+        Console.WriteLine("Gotta gotta be down");
+        Console.WriteLine("Because I want it all"); 
+    }
 }
